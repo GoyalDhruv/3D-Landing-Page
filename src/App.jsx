8ee18c8082a61cdd5a4996521bdfe772f69cea83
Dhruv <dhruvgoyal014@gmail.com>
2025-02-15 22:34:15 +0530

    project setup and herosection made (animations pending)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import HeroSection from './components/HeroSection'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <HeroSection />
+    </main>
   )
 }
 
