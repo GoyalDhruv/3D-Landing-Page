7347fafff88ebfc40c58cdc51c8a0271306562b2
Dhruv <dhruvgoyal014@gmail.com>
2025-02-16 08:39:37 +0530

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from './components/HeroSection'
+import About from './components/About'
 
 function App() {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <HeroSection />
+      <About />
     </main>
   )
 }
